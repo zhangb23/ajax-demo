b8809e4174f0c6f40efe7f3a48767cfb966b74b6
KevinZhang <zhangb23@163.com>
2018-04-06 21:39:36 +0800

    满足Promise规则

--- ajax.js
+++ ajax.js
@@ -1,28 +1,31 @@
 window.JQuery = {}
 window.$ = window.JQuery
-window.JQuery.ajax =function ({url,method,body,success,fail}){
-	let request = new XMLHttpRequest()
-	request.open(method,url)
-    request.onreadystatechange = function () {
-    	if(request.readyState === 4){
-    		if(request.status >= 200 && request.status < 300){
-    			success.call(undefined,request.responseText)
-    		}else if(request.status >= 400){
-    			fail.call(undefined,request)
-    		}
-    	}
-    }
-    request.send(body)
+window.JQuery.ajax =function ({url, method, body}){
+	return new Promise(function(resolve, reject){
+		let request = new XMLHttpRequest()
+		request.open(method,url)
+	    request.onreadystatechange = function () {
+	    	if(request.readyState === 4){
+	    		if(request.status >= 200 && request.status < 300){
+	    			resolve.call(undefined,request.responseText)
+	    		}else if(request.status >= 400){
+	    			reject.call(undefined,request)
+	    		}
+	    	}
+	    }
+	    request.send(body)
+	}
 }
 
 
 
 myButton.addEventListener('click', function(){
-  $.ajax({
-    url: '/xxx',
-    method: 'get',
-    body:'a=1&b=2',
-    success:(x)=>{console.log(x)}
-    fail:(x)=>{console.log(x)}
-  })
+    $.ajax({
+	    url: '/xxx',
+	    method: 'get',
+	    body:'a=1&b=2',
+    }).then(    
+	  	(text)=>{console.log(text)},
+	    (request)=>{console.log(request)}
+	)
 })
